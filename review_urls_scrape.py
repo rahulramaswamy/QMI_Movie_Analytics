8f0c0a88d0f540c858faeb282e047b983e1bc672
Alex Swearingen <alexswearingen26@gmail.com>
2019-11-16 23:52:06 -0600

    scraped a list of urls from metacritic that we can scrape for reviews

---
+++ review_urls_scrape.py
@@ -0,0 +1,25 @@
+import scrapy
+import pandas as pd
+import json
+import re
+import time
+
+class ReviewSpider(scrapy.Spider):
+	name = "reviews"
+	df = pd.read_csv('titles.csv')
+
+	start_urls = []
+	for title in df['title']:
+		title = title.replace(' ', '%20').lower()
+		print(title)
+		start_urls.append(f'https://www.metacritic.com/search/movie/{title}/results')
+
+	def parse(self, response):
+		data = {
+			'title': response.xpath('//*[@id="main_content"]/div[1]/div[3]/div[1]/ul/li/div/div[2]/div/h3/a/text()').extract_first().strip(),
+			'href': response.xpath('//*[@id="main_content"]/div[1]/div[3]/div[1]/ul/li/div/div[2]/div/h3/a/@href').extract_first()
+		}
+		df = pd.DataFrame([data])
+		with open('metacritic_urls.csv', 'a') as fd:
+		   	df.to_csv(fd, mode = 'a', header=False)
+		yield data
