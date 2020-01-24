d09a5ba544a00ad1d915d29d45dfe1edc60fe5ad
Alex Swearingen <alexswearingen26@gmail.com>
2020-01-23 18:19:53 -0600

    got metacritic scores

--- review_urls_scrape.py
+++ review_urls_scrape.py
@@ -11,13 +11,10 @@ class ReviewSpider(scrapy.Spider):
 	start_urls = []
 	for title in df['title']:
 		title = title.replace(' ', '%20').lower()
-		print(title)
 		start_urls.append(f'https://www.metacritic.com/search/movie/{title}/results')
-
 	def parse(self, response):
 		data = {
-			'title': response.xpath('//*[@id="main_content"]/div[1]/div[3]/div[1]/ul/li/div/div[2]/div/h3/a/text()').extract_first().strip(),
-			'href': response.xpath('//*[@id="main_content"]/div[1]/div[3]/div[1]/ul/li/div/div[2]/div/h3/a/@href').extract_first()
+			'url': response.xpath('//*[@id="main_content"]/div[1]/div[3]/div[1]/ul/li[1]/div/div[2]/div/h3/a/@href').extract_first(),
 		}
 		df = pd.DataFrame([data])
 		with open('metacritic_urls.csv', 'a') as fd:
